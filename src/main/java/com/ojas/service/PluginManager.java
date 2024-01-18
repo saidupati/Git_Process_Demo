bac906155545b7ef6fc3c1b5a7b78cc419b112ea
saidupati <saiprasanna.dupati@ojas-it.com>
2024-01-18 15:31:48 +0530

    modified

--- src/main/java/com/ojas/service/PluginManager.java
+++ src/main/java/com/ojas/service/PluginManager.java
@@ -16,10 +16,14 @@ public class PluginManager {
     public void executePlugins() {
         // Added a comment in your master
     	//for conflicts i changed
+        // Added a comment in your saiprasanna
+    	//sai
+    	//prasanna
         // This is a conflicting change in your branch
         System.out.println("Executing plugins in your branch");
 
         for (Plugin plugin : plugins) {
+        	//sai
             plugin.execute();
         }
     }
