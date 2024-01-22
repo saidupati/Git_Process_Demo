853bd7e64a37bc225a8405ecd254f28898af2cde
saidupati <saiprasanna.dupati@ojas-it.com>
2024-01-22 12:48:39 +0530

    changed by sravan

--- src/main/java/com/ojas/service/PluginManager.java
+++ src/main/java/com/ojas/service/PluginManager.java
@@ -19,7 +19,9 @@ public class PluginManager {
         // Added a comment in your saiprasanna
     	//sai
     	//prasann        // This is a conflicting change in your branch
-    	System.out.println("added new line ");
+    	System.out.println("added new line sravan");
+    System.out.println("");
+    	
         System.out.println("Executing plugins in your branch");
 
         for (Plugin plugin : plugins) {
